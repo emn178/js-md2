7a5649ff781aa82b4ed694ea6916fb15bb26abb0
Chen Yi-Cyuan <emn178@gmail.com>
2014-07-27 20:10:10 +0800

    Fixed accents bug

--- src/md2.js
+++ src/md2.js
@@ -1,5 +1,5 @@
 /*
- * js-md5 v0.1.0
+ * js-md5 v0.1.1
  * https://github.com/emn178/js-md2
  *
  * Copyright 2014, emn178@gmail.com
@@ -67,7 +67,7 @@
   
   var hasUTF8 = function(message) {
     for(var i = message.length;i--;)
-      if(message.charCodeAt(i) > 255)
+      if(message.charCodeAt(i) > 127)
         return true;
     return false;
   };
--- tests/test.js
+++ tests/test.js
@@ -2,3 +2,4 @@ assert('md2 1', '8350e5a3e24c153df2275c9f80692773', md2(''));
 assert('md2 2', '03d85a0d629d2c442e987525319fc471', md2('The quick brown fox jumps over the lazy dog'));
 assert('md2 3', '71eaa7e440b611e41a6f0d97384b342a', md2('The quick brown fox jumps over the lazy dog.'));
 assert('md2 4', '7af93c270b0ec392ca2f0d90a927cf8a', md2('中文'));
+assert('md2 5', '628657f2dbd637b6b13500e8567a1c83', md2('aécio'));
